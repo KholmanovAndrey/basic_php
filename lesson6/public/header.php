5d6128617c4326aac35e83255d628d0955aa6ac2
KholmanovAndrey Work <khav1985@gmail.com>
2019-06-24 14:15:30 +0700

    урок 6

---
+++ lesson6/public/header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="public/css/style.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/goods.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/feedback.css" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+<div class="container">
+    <ul class="menu">
+        <li><a href="/">Главная</a></li>
+        <li><a href="/?page=calc">Калькулятор</a></li>
+        <li><a href="/?page=goods">Продукция</a></li>
+        <li><a href="/?page=feedback">Отзывы</a></li>
+    </ul>
+</div>
